b69798feccfb00117075db0fe822054d076024a7
Akshat singh <117257669+akshat2912003@users.noreply.github.com>
2025-07-17 19:42:29 +0530

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA07jsG6vczpFvfsv-mi1rE2-prXKJI1tE",
-  authDomain: "expense-tracker-272ea.firebaseapp.com",
+  apiKey: "*********",
+  authDomain: "*******",
   projectId: "expense-tracker-272ea",
   storageBucket: "expense-tracker-272ea.firebasestorage.app",
-  messagingSenderId: "474728038974",
-  appId: "1:474728038974:web:20015b693d248401c7076f"
+  messagingSenderId: "******",
+  appId: "******"
 };
 
 // Initialize Firebase
